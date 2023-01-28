51e805984ebffb6c03e6d3daa33d342df263188a
no-have-name <123821867+no-have-name@users.noreply.github.com>
2023-01-28 23:38:17 +0800

    pyator1.0.0
    实现了加、减、乘、除、平方、平方2、平方3

---
+++ pyator.py
@@ -0,0 +1,47 @@
+class plus:
+    def plus(a, b):
+        a += b
+        print(a)
+        return a
+
+
+class subtraction:
+    def subtraction(a, b):
+        a -= b
+        print(a)
+        return a
+
+
+class multiplication:
+    def multiplication(a, b):
+        a *= b
+        print(a)
+        return a
+
+
+class division:
+    def division(a, b):
+        a /= b
+        print(a)
+        return a
+
+
+class expo:
+    def expo(a, b):
+        a **= b
+        print(a)
+        return a
+
+
+class sqr:
+    def sqr(a):
+        a **= 2
+        print(a)
+        return a
+
+
+class cube:
+    def cube(a):
+        a **= 3
+        print(a)
+        return a
